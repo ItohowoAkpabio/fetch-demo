3ef752db1ce256fdc1cd3449b08b9fec57b4aa73
ItohowoAkpabio <ityveil@yahoo.co.uk>
2021-07-29 17:05:41 -0400

    added html variable that will render country data inside of country div

---
+++ code.js
@@ -0,0 +1,35 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const countryDiv = document.querySelector(".country-data");
+
+//add event listener to our button //!tab for index pg
+
+button.addEventListener("click", function () {
+  //Grab what the user types into the input box
+  const country = input.value.trim();
+  //fetch out data
+
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const countryData = data[0];
+      console.log(countryData);
+      // const countryData = data; array destructuring
+      // name, flag,region,capital,population
+
+      const html = `
+      <h2>${countryData.name}</h2>
+<img src="${countryData.flag}" alt="flag of ${countryData.name}" title="flag of ${countryData.name}" class="flag">
+
+<h3>${countryData.region}</h3>
+<h4>${countryData.capital}</h4>
+<h4>${countryData.population}</h4>
+`;
+      countryDiv.innerHTML = html;
+
+      // const image = document.createElement("img");
+      // image.src = countryData.flag;
+      // image.classList.add("flag");
+      //countryDiv.append(image);
+    });
+});
